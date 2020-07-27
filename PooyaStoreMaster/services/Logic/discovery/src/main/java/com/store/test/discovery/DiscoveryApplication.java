5e14c6c7a6482f0ff068972e8512663e844129c2
pooyafils <pooyafils@gmail.com>
2020-07-26 17:07:23 -0700

    internal commit

--- PooyaStoreMaster/services/Logic/discovery/src/main/java/com/store/test/discovery/DiscoveryApplication.java
+++ PooyaStoreMaster/services/Logic/discovery/src/main/java/com/store/test/discovery/DiscoveryApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryApplication {
 //check it out later
 	//git chnage inn my local computer
+	//3th git change
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
